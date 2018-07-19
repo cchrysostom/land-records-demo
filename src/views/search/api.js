a3e1392e8b13faa54f3172280ff26b598dfaf9a7
Chris Chrysostom <chris.chrysostom@gmail.com>
2018-07-19 15:43:48 -0600

    Get transaction API

--- src/views/search/api.js
+++ src/views/search/api.js
@@ -2,10 +2,28 @@ import axios from 'axios';
 
 const API_URL = 'https://testnet.flocha.in/api';
 
-export const getTransaction = (data, success, failure) => {
+export const getTransaction = (txid, success, failure) => {
   const API_ENDPOINT = 'tx/'
-  
-  
+  const GET_URL = API_URL + API_ENDPOINT + txid
+  fetch(GET_URL)
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      let tx = {
+        txId: json.txid,
+        size: json.size,
+        fee: json.fees,
+        block: json.blockhash,
+        received: json.time,
+        blocktime: json.blocktime,
+        locktime: json.locktime 
+      }
+      success(tx)
+    })
+    .then((err) => {
+      failure(err)
+    })
 }
 
 export const getAddress = (data, success, failure) => {
